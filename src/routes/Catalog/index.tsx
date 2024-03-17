4795afd2b6fb51e1148694787c528c3463d4f7fc
AYRESRIO <AYRESRIO@HOTMAIL.COM>
2024-03-17 10:58:21 -0300

    Message container.

--- src/routes/Catalog/index.tsx
+++ src/routes/Catalog/index.tsx
@@ -1,9 +1,23 @@
+import "./styles.css";
 import Header from "../../components/Header";
 
-export default function Catalog(){
-
-    return(
-        <Header/>
-
-    );
+export default function Catalog() {
+  return (
+    <>
+      <Header />
+      <main className = "dsct-catalog-main">
+        <section id="dsct-catalog-section">
+          <div className="dsct-message-container">
+            <h1>Venha nos visitar</h1>
+          </div>
+          <div className="dsct-catalog-cards-container">
+            Teste
+            <div>
+              <h3>Lorem ipsum dolor</h3>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
