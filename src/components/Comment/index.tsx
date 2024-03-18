cd45a729945d1957e77587d12e6d58fde6747b4b
AYRESRIO <AYRESRIO@HOTMAIL.COM>
2024-03-17 23:37:28 -0300

    main sections completed.

---
+++ src/components/Comment/index.tsx
@@ -0,0 +1,9 @@
+import "./styles.css";
+export default function Comment(){
+
+    return(
+        <div className = "dsct-comment-border">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae harum voluptatem ad fugit id distinctio.</p>
+          </div>
+    );
+}
--- src/routes/Catalog/index.tsx
+++ src/routes/Catalog/index.tsx
@@ -1,20 +1,32 @@
 import "./styles.css";
 import Header from "../../components/Header";
+import Card from "../../components/Card";
+import Comment from "../../components/Comment";
 
 export default function Catalog() {
   return (
     <>
       <Header />
-      <main className = "dsct-catalog-main">
-        <section id="dsct-catalog-section">
+      <main>
+        <section id="dsct-catalog-section" className="dsct-catalog-main">
           <div className="dsct-message-container">
             <h1>Venha nos visitar</h1>
           </div>
           <div className="dsct-catalog-cards-container">
-            Teste
-            <div>
-              <h3>Lorem ipsum dolor</h3>
-            </div>
+            <Card />
+            <Card />
+          </div>
+        </section>
+        <section id="dsct-catalog-comments-section">
+          <div className="dsct-title-container">
+            <h1>O que estão dizendo</h1>
+          </div>
+          <div>
+            <Comment />
+            <Comment />
+            <Comment />
+            <Comment />
+            <Comment />
           </div>
         </section>
       </main>
